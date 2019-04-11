cf92a921e65e51b9abb7a2c47599132f0612c67c
antonina palchikova <aapalchikova@avito.ru>
2019-04-12 00:04:14 +0300

    normal has been done

--- 5_hard_1.py
+++ 5_hard_1.py
@@ -14,3 +14,48 @@
 # Исходной директорией считать ту, в которой был запущен скрипт.
 
 # P.S. По возможности, сделайте кросс-платформенную реализацию.
+
+
+
+
+# Узнать текущий каталог:
+# os.getcwd()
+# '/home/pl/Documents/python'
+
+# Создать каталог:
+#
+# >>> os.mkdir('TEXT')
+# Создать дерево каталогов:
+#
+# >>> os.makedirs('ONE/TWO/THREE')
+# >>> os.listdir('ONE')
+# ['TWO']
+# >>> os.listdir('ONE/TWO')
+# ['THREE']
+# Список содержимого каталога (нерекурсивный):
+#
+# >>> os.listdir()
+# ['text4.txt', 'text1.txt', 'text3.txt', 'try_except.py', 'files.py', 'text5.txt', 'grades_oop.py', 'grades.py', 'text6.txt', 'text2.txt', 'text.txt', 'TEXT']
+# >>> os.listdir('/home')
+# ['pl']
+# Сведения о файле|каталоге:
+#
+# >>> os.stat('/home')
+# posix.stat_result(st_mode=16877, st_ino=1048577, st_dev=2053, st_nlink=3, st_uid=0, st_gid=0, st_size=4096, st_atime=1344368518, st_mtime=1339316982, st_ctime=1339316982)
+# >>> os.stat('text.txt')
+# posix.stat_result(st_mode=33188, st_ino=1575740, st_dev=2053, st_nlink=1, st_uid=1000, st_gid=1000, st_size=41, st_atime=1344124896, st_mtime=1343895362, st_ctime=1343895362)
+# Переименовать файл|каталог:
+#
+# >>> os.rename('text2.txt', 'xett.txt')
+# >>> os.rename('TEXT', 'ETXT')
+# Удаление каталога:
+#
+# >>> os.rmdir('ETXT')
+# Смена текущего каталога:
+#
+# >>> os.chdir('/home')
+# >>> os.getcwd()
+# '/home'
+# >>> os.chdir('./pl/Documents/python')
+# >>> os.getcwd()
+'/home/pl/Documents/python'
--- 5_normal_1.py
+++ 5_normal_1.py
@@ -1,3 +1,7 @@
+# toneypal - Antonina Palchikova
+# Программа робот-помощник-информатор
+
+
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +17,83 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+
+# import os
+# import sys
+# import platform
+
+import os.path
+
+answer = ''
+FILE_LIST_IN_DIR = os.listdir()
+DICT_CHOICES = {
+    "0": "Узнать текущий каталог:",
+    "1": "Перейти в папку",
+    "2": "Просмотреть содержимое текущей папки",
+    "3": "Создать папку",
+    "4": "Удалить папку"
+}
+
+while answer != 'q':
+    answer = input( "\n хотите поработать? [y/n/q]" )
+
+    try:
+        if answer == 'y':
+            print( "Тогда выберете из списка:" )
+            for key, action in DICT_CHOICES.items():
+                print( "__________________", key, action )
+
+            choice = input( "Введите ваш выбор:" )
+            print( type( int( choice ) ), type( choice ) )
+            if choice in DICT_CHOICES.keys():
+
+                if int( choice ) == 0:
+                    print( "Узнать текущий каталог: ", os.getcwd())
+
+                elif int( choice ) == 1:
+                    print( "Введите путь к папке в формате: ")
+                    dir_name = input()
+
+                    if os.path.exists( dir_name ):
+                        os.chdir( dir_name )
+                        print( "Текущий каталог: ", os.getcwd())
+
+                elif int( choice ) == 2:
+                    for elem in os.listdir( os.getcwd()):
+                        print( "__________________", elem)
+
+                elif int( choice ) == 3:
+                    new_dir = input( "Введите имя директории: ")
+                    new_dir_path = input( "Введите путь, где будет создана указанная директория: ")
+                    if not os.path.exists( new_dir_path + new_dir ):
+                        os.chdir( new_dir_path)
+                        try:
+                            os.mkdir( new_dir)
+                            print( "Успешно")
+                        except Exception as e:
+                            print( e)
+
+                elif int( choice ) == 4:
+                    print( "Какую папку Вы хотели бы удалить? Введите путь до этой папки: ")
+                    dir_del_path = input( "Введите путь до этой папки: ")
+                    if os.path.exists( dir_del_path ):
+                        try:
+                            os.remove( dir_del_path )
+                            print( "Файл успешно удален" )
+                        except Exception as e:
+                            print( e )
+                    else:
+                        print( f'Я не смог найти файл {dir_del_path}')
+
+        elif answer == 'n':
+            print( "Good bay!" )
+            exit()
+        elif answer == 'q':
+            exit()
+        else:
+            print( "Answer is not recognized" )
+
+    except Exception as e:
+        print( "Некорректный ввод!" )
+        print( f'e = {e} /n trace: {e.with_traceback()}')
